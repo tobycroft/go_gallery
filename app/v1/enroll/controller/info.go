12962585cc48d85964f227442bcda548a00910a5
火线兔 <silverhawk@foxmail.com>
2023-03-15 13:25:33 +0800

    TuuzMac

--- app/v1/enroll/controller/info.go
+++ app/v1/enroll/controller/info.go
@@ -1,7 +1,72 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"main.go/common/BaseController"
+	"main.go/tuuz/Input"
+)
 
 func EnrollController(route *gin.RouterGroup) {
+
+	route.Use(BaseController.LoginedController(), gin.Recovery())
+
+	route.Any("add", enroll_add)
+
+}
+
+func enroll_add(c *gin.Context) {
+	uid := c.GetHeader("uid")
+	tag_id, ok := Input.PostInt64("tag", c)
+	if !ok {
+		return
+	}
+	age, ok := Input.PostInt64("age", c)
+	if !ok {
+		return
+	}
+	tag_group_id, ok := Input.PostInt64("tag_group_id", c)
+	if !ok {
+		return
+	}
+	name, ok := Input.Post("name", c, true)
+	if !ok {
+		return
+	}
+	email, ok := Input.Post("email", c, true)
+	if !ok {
+		return
+	}
+	gender, ok := Input.PostInt64("gender", c)
+	if !ok {
+		return
+	}
+	cert, ok := Input.Post("cert", c, true)
+	if !ok {
+		return
+	}
+	school_name, ok := Input.Post("school_name", c, true)
+	if !ok {
+		return
+	}
+	phone, ok := Input.Post("phone", c, true)
+	if !ok {
+		return
+	}
+	province, ok := Input.Post("province", c, true)
+	if !ok {
+		return
+	}
+	city, ok := Input.Post("city", c, true)
+	if !ok {
+		return
+	}
+	district, ok := Input.Post("district", c, true)
+	if !ok {
+		return
+	}
+	address, ok := Input.Post("address", c, true)
+	if !ok {
+		return
+	}
 
 }
--- app/v1/enroll/model/EnrollModel/EnrollModel.go
+++ app/v1/enroll/model/EnrollModel/EnrollModel.go
@@ -1,3 +1,89 @@
 package EnrollModel
 
+import (
+	"github.com/tobycroft/gorose-pro"
+	"main.go/tuuz"
+	"main.go/tuuz/Log"
+)
+
 const Table = "g_enroll"
+
+type Interface struct {
+	Db gorose.IOrm
+}
+
+func Api_insert(uid, tag_id, age, tag_group_id, name, email, gender, cert, school_name, phone, province, city, district, address interface{}) bool {
+	db := tuuz.Db().Table(Table)
+	data := map[string]interface{}{
+		"uid":          uid,
+		"tag_id":       tag_id,
+		"age":          age,
+		"tag_group_id": tag_group_id,
+		"name":         name,
+		"email":        email,
+		"gender":       gender,
+		"cert":         cert,
+		"school_name":  school_name,
+		"phone":        phone,
+		"province":     province,
+		"city":         city,
+		"district":     district,
+		"address":      address,
+	}
+	db.Data(data)
+	_, err := db.Insert()
+	if err != nil {
+		Log.DBrrsql(err, db, tuuz.FUNCTION_ALL())
+		return false
+	} else {
+		return true
+	}
+}
+
+func Api_update_isUsed(order_id, is_used interface{}) bool {
+	db := tuuz.Db().Table(Table)
+	db.Where("order_id", order_id)
+	data := map[string]any{
+		"is_used": is_used,
+	}
+	db.Data(data)
+	_, err := db.Update()
+	if err != nil {
+		Log.DBrrsql(err, db, tuuz.FUNCTION_ALL())
+		return false
+	} else {
+		return true
+	}
+}
+
+func Api_update_isVerify(order_id, is_verify interface{}) bool {
+	db := tuuz.Db().Table(Table)
+	db.Where("order_id", order_id)
+	data := map[string]any{
+		"is_verify": is_verify,
+	}
+	db.Data(data)
+	_, err := db.Update()
+	if err != nil {
+		Log.DBrrsql(err, db, tuuz.FUNCTION_ALL())
+		return false
+	} else {
+		return true
+	}
+}
+
+func Api_update_isPayed(order_id, is_payed interface{}) bool {
+	db := tuuz.Db().Table(Table)
+	db.Where("order_id", order_id)
+	data := map[string]any{
+		"is_payed": is_payed,
+	}
+	db.Data(data)
+	_, err := db.Update()
+	if err != nil {
+		Log.DBrrsql(err, db, tuuz.FUNCTION_ALL())
+		return false
+	} else {
+		return true
+	}
+}
