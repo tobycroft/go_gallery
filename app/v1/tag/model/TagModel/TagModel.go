6ee68d1e41e66618197451aa299da25740839781
火线兔 <silverhawk@foxmail.com>
2023-03-15 11:30:55 +0800

    TuuzMac

--- app/v1/tag/model/TagModel/TagModel.go
+++ app/v1/tag/model/TagModel/TagModel.go
@@ -18,20 +18,3 @@ func Api_select() []gorose.Data {
 		return ret
 	}
 }
-
-func Api_select_byType(study_type, tag_type interface{}) []gorose.Data {
-	db := tuuz.Db().Table(Table)
-	if study_type != nil {
-		db.Where("study_type", study_type)
-	}
-	if tag_type != nil {
-		db.Where("tag_type", tag_type)
-	}
-	ret, err := db.Get()
-	if err != nil {
-		Log.Dbrr(err, tuuz.FUNCTION_ALL())
-		return nil
-	} else {
-		return ret
-	}
-}
