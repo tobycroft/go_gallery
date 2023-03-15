2dfc3648036b2769a6a918b8643155a55f6ba515
火线兔 <silverhawk@foxmail.com>
2023-03-15 13:16:03 +0800

    TuuzMac

--- route/v1/enrollRouter.go
+++ route/v1/enrollRouter.go
@@ -2,11 +2,14 @@ package v1
 
 import (
 	"github.com/gin-gonic/gin"
+	"main.go/app/v1/enroll/controller"
 )
 
 func EnrollRouter(route *gin.RouterGroup) {
 	route.Any("/", func(context *gin.Context) {
 		context.String(0, route.BasePath())
 	})
+
+	controller.EnrollController(route.Group("enroll"))
 
 }
