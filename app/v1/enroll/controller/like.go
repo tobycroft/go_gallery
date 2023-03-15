d72b6214b740c4779b8bd266a35552418c1b0da2
火线兔 <silverhawk@foxmail.com>
2023-03-15 17:01:19 +0800

    TuuzMac

--- app/v1/enroll/controller/like.go
+++ app/v1/enroll/controller/like.go
@@ -26,12 +26,12 @@ func like_add(c *gin.Context) {
 	like_limit := SystemParamModel.Api_find_val("like_limit")
 	like_limit_day := SystemParamModel.Api_find_val("like_limit_day")
 	count := EnrolllikeModel.Api_count(uid, enroll_id)
-	if count > Calc.Any2Int64(like_limit) {
+	if count >= Calc.Any2Int64(like_limit) {
 		RET.Fail(c, 406, nil, "超过投票次数")
 		return
 	}
 	count_day := EnrolllikeModel.Api_count_today(uid)
-	if count_day > Calc.Any2Int64(like_limit_day) {
+	if count_day >= Calc.Any2Int64(like_limit_day) {
 		RET.Fail(c, 406, nil, "超过当日投票次数")
 		return
 	}
