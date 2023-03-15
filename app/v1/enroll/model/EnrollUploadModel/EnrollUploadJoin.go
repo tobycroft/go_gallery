2319588206e4bd20ecaa1f5e50478a7b93b72b9f
火线兔 <silverhawk@foxmail.com>
2023-03-15 17:13:34 +0800

    TuuzMac

--- app/v1/enroll/model/EnrollUploadModel/EnrollUploadJoin.go
+++ app/v1/enroll/model/EnrollUploadModel/EnrollUploadJoin.go
@@ -9,7 +9,7 @@ import (
 
 func Api_joinEnroll_paginator_byTagId(tag_id any, limit, page int) gorose.Paginate {
 	db := tuuz.Db().Table(Table + " a")
-	db.Fields("a.enroll_id", "a.attachment", "a.title", "b.tag_id", "b.name")
+	db.Fields("a.enroll_id", "a.attachment", "a.title", "b.tag_id", "b.name", "a.likes")
 	db.LeftJoin(EnrollModel.Table+" b", "a.enroll_id=b.id")
 	db.Where("tag_id", tag_id)
 	db.Limit(limit)
@@ -26,7 +26,7 @@ func Api_joinEnroll_paginator_byTagId(tag_id any, limit, page int) gorose.Pagina
 
 func Api_joinEnroll_paginator_byTagId_orderByLikes(tag_id any, limit, page int) gorose.Paginate {
 	db := tuuz.Db().Table(Table + " a")
-	db.Fields("a.enroll_id", "a.attachment", "a.title", "b.tag_id", "b.name")
+	db.Fields("a.enroll_id", "a.attachment", "a.title", "b.tag_id", "b.name", "a.likes")
 	db.LeftJoin(EnrollModel.Table+" b", "a.enroll_id=b.id")
 	db.Where("tag_id", tag_id)
 	db.Limit(limit)
