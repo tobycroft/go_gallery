0c76b87325fe4fce80cc36d0008a7edc1f3fe316
火线兔 <silverhawk@foxmail.com>
2023-03-23 17:44:06 +0800

    TuuzMac

--- app/v1/wechat/controller/pay.go
+++ app/v1/wechat/controller/pay.go
@@ -7,6 +7,7 @@ import (
 	"github.com/wechatpay-apiv3/wechatpay-go/core"
 	"github.com/wechatpay-apiv3/wechatpay-go/core/option"
 	"github.com/wechatpay-apiv3/wechatpay-go/services/certificates"
+	"github.com/wechatpay-apiv3/wechatpay-go/services/payments/jsapi"
 	"github.com/wechatpay-apiv3/wechatpay-go/utils"
 	"golang.org/x/net/context"
 	"log"
@@ -18,6 +19,7 @@ func PayController(route *gin.RouterGroup) {
 	route.Use(BaseController.LoginedController(), gin.Recovery())
 
 	route.Any("index", pay_index)
+	route.Any("order", pay_order)
 
 }
 
@@ -50,6 +52,8 @@ func _ready() {
 	}
 }
 
+var ctx context.Context
+
 func pay_index(c *gin.Context) {
 	// 使用 utils 提供的函数从本地文件中加载商户私钥，商户私钥会用来生成请求的签名
 	mchPrivateKey, err := utils.LoadPrivateKeyWithPath("./apiclient_key.pem")
@@ -57,19 +61,52 @@ func pay_index(c *gin.Context) {
 		log.Fatal("load merchant private key error")
 	}
 
-	ctx := context.Background()
-	// 使用商户私钥等初始化 client，并使它具有自动定时获取微信支付平台证书的能力
+	ctx = context.Background()
+	// 使用商户私钥等初始化 clients，并使它具有自动定时获取微信支付平台证书的能力
 	opts := []core.ClientOption{
 		option.WithWechatPayAutoAuthCipher(mchID, mchCertificateSerialNumber, mchPrivateKey, mchAPIv3Key),
 	}
-	client, err := core.NewClient(ctx, opts...)
+	clients, err := core.NewClient(ctx, opts...)
 	if err != nil {
-		log.Fatalf("new wechat pay client err:%s", err)
+		log.Fatalf("new wechat pay clients err:%s", err)
 	}
 
 	// 发送请求，以下载微信支付平台证书为例
 	// https://pay.weixin.qq.com/wiki/doc/apiv3/wechatpay/wechatpay5_1.shtml
-	svc := certificates.CertificatesApiService{Client: client}
+	svc := certificates.CertificatesApiService{Client: clients}
 	resp, result, err := svc.DownloadCertificates(ctx)
+	client = clients
 	log.Printf("status=%d resp=%s", result.Response.StatusCode, resp)
+}
+
+var client *core.Client
+
+func pay_order(c *gin.Context) {
+
+	svc := jsapi.JsapiApiService{Client: client}
+	// 得到prepay_id，以及调起支付所需的参数和签名
+	resp, result, err := svc.PrepayWithRequestPayment(ctx,
+		jsapi.PrepayRequest{
+			Appid:       core.String("wxd678efh567hg6787"),
+			Mchid:       core.String("1900009191"),
+			Description: core.String("Image形象店-深圳腾大-QQ公仔"),
+			OutTradeNo:  core.String("1217752501201407033233368018"),
+			Attach:      core.String("自定义数据说明"),
+			NotifyUrl:   core.String("https://www.weixin.qq.com/wxpay/pay.php"),
+			Amount: &jsapi.Amount{
+				Total: core.Int64(100),
+			},
+			Payer: &jsapi.Payer{
+				Openid: core.String("oUpF8uMuAJO_M2pxb1Q9zNjWeS6o"),
+			},
+		},
+	)
+
+	fmt.Println(result)
+
+	if err == nil {
+		log.Println(resp)
+	} else {
+		log.Println(err)
+	}
 }
