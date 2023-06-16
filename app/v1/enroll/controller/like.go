f98d64cab6059838d77cc4d8c43d7ff774133add
火线兔 <silverhawk@foxmail.com>
2023-06-16 17:28:10 +0800

    TuuzMac

--- app/v1/enroll/controller/like.go
+++ app/v1/enroll/controller/like.go
@@ -18,6 +18,7 @@ func LikeController(route *gin.RouterGroup) {
 	route.Use(BaseController.LoginedController(), gin.Recovery())
 
 	route.Any("add", like_add)
+	route.Any("status", like_status)
 }
 
 func like_add(c *gin.Context) {
@@ -58,7 +59,7 @@ func like_add(c *gin.Context) {
 	RET.Success(c, 0, nil, nil)
 }
 
-func like_avail(c *gin.Context) {
+func like_status(c *gin.Context) {
 	uid := c.GetHeader("uid")
 	enroll_id, ok := Input.PostInt64("enroll_id", c)
 	if !ok {
@@ -67,15 +68,7 @@ func like_avail(c *gin.Context) {
 	like_limit := SystemParamModel.Api_find_val("like_limit")
 	like_limit_day := SystemParamModel.Api_find_val("like_limit_day")
 	count := EnrolllikeModel.Api_count(uid, enroll_id)
-	if count >= Calc.Any2Int64(like_limit) {
-		RET.Fail(c, 406, nil, "超过投票次数")
-		return
-	}
 	count_day := EnrolllikeModel.Api_count_today(uid)
-	if count_day >= Calc.Any2Int64(like_limit_day) {
-		RET.Fail(c, 406, nil, "超过当日投票次数")
-		return
-	}
 	RET.Success(c, 0, map[string]any{
 		"like_sum":       count,
 		"like_today":     count_day,
